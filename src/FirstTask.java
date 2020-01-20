55a2c89dcd79d1d15e3d90c0a4c74f3f06aa036c
maxij <maximgri93@gmail.com>
2020-01-20 20:48:01 +0300

    Maxim, when I write 0, my program is working.

---
+++ src/FirstTask.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class FirstTask {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int radius = sc.nextInt();
+        if (radius < 0) {
+            System.out.println("Radius can't be negative");
+        }
+        else {
+            double pi = 3.14;
+            double pRound = 2 * radius * pi;
+            double sRound = pi * Math.pow(radius, 2);
+            System.out.println(pRound);
+            System.out.println(sRound);
+        }
+        if (radius == 0) {
+            System.out.println("It's point");
+        }
+    }
+}
