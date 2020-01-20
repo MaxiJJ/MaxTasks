08ffbd19507a4a39e2e47468f6bac22114f4a95d
maxij <maximgri93@gmail.com>
2020-01-20 21:40:19 +0300

    Refactor code

--- src/FirstTask.java
+++ src/FirstTask.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class FirstTask {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int radius = sc.nextInt();
         if (radius < 0) {
